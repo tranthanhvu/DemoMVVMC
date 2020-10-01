ba05b4f5e32548b69e11caee9b4d5e2945e2baa2
vutt <>
2020-10-01 13:13:21 +0700

    first commit

---
+++ DemoMVVMC/DemoMVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoMVVMC
+//
+//  Created by ttvu on 10/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
